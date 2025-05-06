7dfd69216b7835fd91b10218705f2506ba2db548
jcecena <jantonio.cecena@gmail.com>
2025-05-06 10:12:31 -0700

    Se creó el proyecto y se agregaron clases de entidad

---
+++ src/main/java/mx/itson/grillo/entities/Forecast.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mx.itson.grillo.entities;
+
+import java.util.Date;
+import mx.itson.grillo.enums.WeatherStatus;
+
+/**
+ *
+ * @author janto
+ */
+public class Forecast {
+    
+    private Date day;
+    private WeatherStatus status;
+    private int maxTemperature;
+    private int minTemperature;
+}
